4086a67568e11aee1fae1d82d67ac38b553f9a91
MohmedIkram <53212527+MohmedIkram@users.noreply.github.com>
2021-07-24 21:19:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// const directory = "./folder1";
-// import { readdir } from "fs";
-
-// readdir(directory, (err, files) => {
-//   files.forEach((file) => {
-//     console.log(file);
-//   });
-// });
-
 import express from "express";
-import { readdir } from "fs";
-import "./images/myimg.png";
-
 const app = express();
-//directory path
-const directory = "./folder1";
-const PORT = 3000;
+import { statSync, readdir } from "fs";
+import emoji from "node-emoji";
+var content = "";
 
-app.get("/", (request, respone) => {
-  readdir(directory, (err, files) => {
-    files.forEach((file) => {
-      respone.send(file + "/index.html");
-    });
-  });
+content = `list of files in my loocal storage(desktop files )`;
+let foo = (err, files) => {
+  let fl = files.length;
+  for (let i = 0; i < fl; i++) {
+    var stats = statSync(`C:/Users/ikram/Desktop/${files[i]}`);
+    if (stats.isFile()) {
+      //If it is as file
+      content += `
+      ${emoji.emojify(":open_file_folder:")}file ~ ${files[i]}`;
+    } else {
+      //If it is a directory
+      content += ` 
+      ${emoji.emojify(":star:")}directory ~ ${files[i]}`;
+    }
+  }
+};
+readdir("C:/Users/ikram/Desktop/", "utf8", foo);
+app.get("/", (req, res) => {
+  res.send(content);
 });
-
-app.listen(PORT, () => console.log("server is started"));
+app.listen(3000, () => console.log("server is started"));
